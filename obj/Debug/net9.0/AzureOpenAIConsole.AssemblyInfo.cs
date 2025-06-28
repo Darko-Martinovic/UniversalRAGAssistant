d37bf258c663a13f4f6791c13a72cce6219f0ca1
Darko Martinovic <darko.martinovic@outlook.com>
2025-06-28 16:07:35 +0200

    s

--- obj/Debug/net9.0/AzureOpenAIConsole.AssemblyInfo.cs
+++ obj/Debug/net9.0/AzureOpenAIConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureOpenAIConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d7b953fcca0fec543e88782d1ebebdbe065d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureOpenAIConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureOpenAIConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
